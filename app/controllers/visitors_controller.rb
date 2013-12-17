84c5b2e24f3c5db2b68a5b7f3d78aa848ef611c9
pgarber <pgarber108@gmail.com>
2013-12-17 13:37:39 -0500

    update application layotu

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,5 +3,7 @@ class VisitorsController < ApplicationController
     @owner = Owner.new  # @owner is usable in view file because of @
     # there is hidden code from rails that also does:
     # render 'visitors/new'
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My birthday is soon.'
   end
 end
