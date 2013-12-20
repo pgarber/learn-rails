239fffb5c28c70999d9780796a57dc44af34a9c3
pgarber <pgarber108@gmail.com>
2013-12-19 19:10:58 -0500

    slight tweak for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,20 @@ LearnRails::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # email enabled in production
+  config.action_mailer.default_url_options = { :host => ENV["pmg2-learn-rails.herokuapp.com"] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["pmg2-learn-rails.herokuapp.com"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["philip.m.garber@gmail.com"],
+    password: ENV["luckyink932"]
+  }
 end
